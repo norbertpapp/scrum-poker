e874515fa20e097f9aa03027689c15ddc86c6911
norbertpapp <186974+norbertpapp@users.noreply.github.com>
2026-01-12 16:16:36 +0100

    Updated default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,6 @@
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between items-center py-4">
           <div class="flex items-center space-x-3">
-            <img src="/logo.png" width="40px" height="40px" title="Scrum Poker" />
             <div class="w-10 h-10 bg-gradient-to-r from-primary-500 to-purple-600 rounded-lg flex items-center justify-center">
               <span class="text-white font-bold text-lg">SP</span>
             </div>
