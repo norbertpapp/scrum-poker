ed9279a97f69433789f823982efccf24249bc70f
norbertpapp <186974+norbertpapp@users.noreply.github.com>
2026-01-12 16:11:15 +0100

    Updated nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
         lang: 'en',
       },
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' },
+        { rel: 'icon', type: 'image/x-icon', href: './favicon.png' },
       ]
     }
   },
