198d492b0a6b10bf5fd64215fa54abfdf1baccfa
norbert.papp-lpg <norbert.papp@team.blue>
2026-04-30 00:24:39 +0200

    refactor: shorten leave room button text

--- layouts/default.vue
+++ layouts/default.vue
@@ -59,7 +59,7 @@
               class="btn-secondary h-10 text-red-600 hover:bg-red-50"
               @click="handleLeaveRoom"
             >
-              ❌ Leave Room
+              ❌ Leave
             </button>
           </div>
         </div>
