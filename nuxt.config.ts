1c54f6e99b9bf111f59aa672427abfdc068fe643
norbert.papp-lpg <norbert.papp@team.blue>
2026-04-30 00:11:33 +0200

    refactor: migrate from Tailwind CSS to UnoCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@unocss/nuxt'],
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     public: {
